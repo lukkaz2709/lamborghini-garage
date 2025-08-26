bc54dd462a52f20787042434a97bfe9cc39b50b1
lukkaz2709 <lmiguez2709@gmail.com>
2025-08-26 14:04:56 -0300

    project API Lamborghini create!

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import GarageScreen from './src/screens/GarageScreen';
 
 export default function App() {
